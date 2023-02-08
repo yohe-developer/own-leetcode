56adfc2b253a7cddfa4c712a6b04fae43d426fd6
aio <aioet@qq.com>
2023-02-08 10:18:52 +0800

    feat: 20230208

---
+++ 2023/704.二分查找.ts
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=704 lang=typescript
+ *
+ * [704] 二分查找
+ */
+
+// @lc code=start
+function search(nums: number[], target: number): number {
+
+  let start = 0
+  let end = nums.length
+
+  while (start <= end) {
+    let mid = Math.floor((start + end) / 2)
+    let value = nums[mid]
+    if (value === target) {
+      return mid
+    } else if (value > target) {
+      end = mid - 1
+    } else {
+      start = mid + 1
+    }
+  }
+  return -1
+};
+// @lc code=end
+
