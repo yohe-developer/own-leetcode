760b243a5665148428bf50072722b62ee2df1afd
aio <aioet@qq.com>
2023-02-24 08:59:52 +0800

    feat: 0223

---
+++ 2023/03.ts
@@ -0,0 +1,15 @@
+function findRepeatNumber(nums: number[]): number {
+    const temp:any = {}
+    for(let i = 0 ; i< nums.length; i++){
+        if(!temp[nums[i]]){
+            temp[nums[i]] = 0
+        }
+        temp[nums[i]]+=1
+        if(temp[nums[i]]>1){
+            return nums[i]
+        } 
+    }
+    return -1
+};
+
+findRepeatNumber([2, 3, 1, 0, 2, 5, 3])
